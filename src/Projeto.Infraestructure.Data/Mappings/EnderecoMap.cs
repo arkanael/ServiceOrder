205f24e0130f0c6c135bd37dc773a4a8c13fd7dc
Raphael Duarte <raphael-duarte@outlook.com>
2021-01-24 14:33:41 -0300

    Criado o mapeamento da classe Endereço.

---
+++ src/Projeto.Infraestructure.Data/Mappings/EnderecoMap.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Projeto.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Infraestructure.Data.Mappings
+{
+    public class EnderecoMap : IEntityTypeConfiguration<Endereco>
+    {
+        void IEntityTypeConfiguration<Endereco>.Configure(EntityTypeBuilder<Endereco> builder)
+        {
+            builder.ToTable("Endereco");
+
+            builder.HasKey(x => x.IdEndereco);
+
+            builder.Property(x => x.Logradouro)
+                .HasColumnType("varchar")
+                .HasMaxLength(250)
+                .IsRequired();
+
+            builder.Property(x => x.Numero)
+                .HasColumnType("varchar")
+                .HasMaxLength(10000)
+                .IsRequired();
+
+            builder.Property(x => x.Bairro)
+                .HasColumnType("varchar")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(x => x.Estado)
+                .HasColumnType("varchar")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.Cidade)
+                .HasColumnType("varchar")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.Complemento)
+                .HasColumnType("varchar")
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.CEP)
+                .HasColumnType("varchar")
+                .HasMaxLength(8)
+                .IsRequired();
+        }  
+    }
+}
