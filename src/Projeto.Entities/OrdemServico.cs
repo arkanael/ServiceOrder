a1453b4824ab1270543d72615eecef59fb4599dc
Fábio Cavadas <fabiocfferreira@gmail.com>
2021-01-24 12:04:24 -0300

    Add Entitie OrdemServico

---
+++ src/Projeto.Entities/OrdemServico.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Entities
+{
+    public class OrdemServico
+    {
+        public Guid IdOrdemServico { get; set; }
+    }
+}
