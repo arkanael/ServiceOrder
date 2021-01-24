aaf32ac70674c4a7b90860dfb47d67e9bb48ab48
Raphael Duarte <raphael-duarte@outlook.com>
2021-01-24 11:48:26 -0300

    Criado a classe endereço.

---
+++ src/Projeto.Entities/Endereco.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Entities
+{
+    public class Endereco
+    {
+        public int MyProperty { get; set; }
+    }
+}
