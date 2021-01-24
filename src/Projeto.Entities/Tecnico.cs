4e2db535b356b943a80e2e7d74dc384223be0b4d
Fábio Cavadas <fabiocfferreira@gmail.com>
2021-01-24 12:29:36 -0300

    Add Entitie Tecnico

---
+++ src/Projeto.Entities/Tecnico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Entities
+{
+    public class Tecnico
+    {
+        public Guid IdTecnico { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+    }
+}
